97a17c410c505bb267ca7c6c07e589f038213427
Corpelijn <bassiecorpelijn@hotmail.com>
2015-11-12 18:07:32 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Row The Boat/Assets/Prefabs/Riverbank empty.prefab.meta

---
+++ Row The Boat/Assets/Scripts/RowTiltController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+
+public class RowTiltController : MonoBehaviour
+{
+    private Vector3 _acceleration;
+    private float _accumulatedStrength;
+    private float _efficiency;
+    private Vector3 _rotation;
+    private RowSide _rowSide;
+
+    public event EventHandler<RowEventArgs> Row;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _rotation = SensorHelper.rotation.eulerAngles;
+
+        CheckRowSide();
+        CheckEfficiency();
+        CheckRowMotion();
+    }
+
+    public void CheckRowSide()
+    {
+        if (_rotation.z >= 15 && _rotation.z <= 60)
+        {
+            _rowSide = RowSide.Left;
+        }
+        else if (_rotation.z <= 345 && _rotation.z >= 300)
+        {
+            _rowSide = RowSide.Right;
+        }
+        else
+        {
+            _rowSide = RowSide.None;
+        }
+    }
+
+    public void CheckEfficiency()
+    {
+        if (_rowSide != RowSide.None)
+        {
+            if (_rotation.x >= 270)
+            {
+                _efficiency = ((_rotation.x - 270) * (100f / 90f)) / 100f;
+            }
+            else if (_rotation.x <= 90)
+            {
+                _efficiency = Mathf.Abs(((_rotation.x - 90) * (100f / 90f)) / 100f);
+            }
+            else
+            {
+                _efficiency = 0;
+            }
+        }
+        else
+        {
+            _efficiency = 0;
+        }
+    }
+
+    public void CheckRowMotion()
+    {
+        _acceleration = Input.gyro.userAcceleration;
+        if (_acceleration.z >= 0.05f)
+        {
+            _accumulatedStrength += _acceleration.z;
+        }
+        else if (_accumulatedStrength >= 0.3f)
+        {
+            OnRow(new RowEventArgs(_rowSide, _accumulatedStrength, _efficiency));
+            _accumulatedStrength = 0;
+        }
+    }
+
+    protected virtual void OnRow(RowEventArgs e)
+    {
+        var row = Row;
+        if (row != null) row.Invoke(this, e);
+    }
+
+    public class RowEventArgs : EventArgs
+    {
+        public RowEventArgs(RowSide side, float strength, float efficiency)
+        {
+            Side = side;
+            Strength = strength;
+            Efficiency = efficiency;
+        }
+
+        public RowSide Side { get; protected set; }
+        public float Strength { get; protected set; }
+        public float Efficiency { get; protected set; }
+    }
+
+    public enum RowSide
+    {
+        Left,
+        Right,
+        None
+    }
+}
---
+++ Row The Boat/Assets/Scripts/SliderChangeOn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class SliderChangeOn : MonoBehaviour
+{
+	[SerializeField]
+	private Slider _slider;
+
+	[SerializeField]
+	private Text _multiplierText;
+
+	[SerializeField]
+	private Roeiboot _roeiboot;
+
+	public void OnSliderValueChanged()
+	{
+		this._roeiboot.ForceMultiplier = this._slider.value;
+		this._multiplierText.text = this._roeiboot.ForceMultiplier.ToString();
+	}
+
+}
