2d4a6e28edd6f4fe3911a105e8d0e529d851a892
KittyvdP <kittyvdp@gmail.com>
2015-11-13 14:18:29 +0100

    creaking :boat:

--- Row The Boat/Assets/Scripts/WaterOnCam.cs
+++ Row The Boat/Assets/Scripts/WaterOnCam.cs
@@ -7,24 +7,34 @@ public class WaterOnCam : MonoBehaviour {
     public RectTransform canvas;
     public GameObject gameover;
     private bool moving;
+    private bool alive;
     private Vector3 velocity = Vector3.zero;
     [SerializeField]
     private float smoothTime = 0.3f;
     private Vector3 originalPosition;
+    [SerializeField]
+    private AudioSource creaking;
+    [SerializeField]
+    private AudioSource sinking;
 
 	// Use this for initialization
 	void Start () {
         moving = false;
+        alive = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.C) && alive)
             RaiseWater();
 
         if (transform.position.y >= canvas.GetComponent<RectTransform>().sizeDelta.y)
+        {
             gameover.SetActive(true);
+            alive = false;
+        }
+            
 
         if (moving)
         {
@@ -32,10 +42,17 @@ public class WaterOnCam : MonoBehaviour {
             if (transform.position.y >= originalPosition.y + 40)
                 moving = false;
         }
+
+        if (!alive)
+        {
+            sinking.Play();
+        }
+            
 	}
 
     public void RaiseWater()
     {
+        creaking.Play();
         originalPosition = transform.position;
         moving = true;
         //transform.position = new Vector3(transform.position.x, transform.position.y + 40, transform.position.z);
