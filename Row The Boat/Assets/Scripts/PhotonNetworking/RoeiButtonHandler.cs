2efe49de622a167df8fde7c76c43a832198f8461
Lorenzo <sheepytaggernep@gmail.com>
2015-11-13 12:07:07 +0100

    Roei button toegevoegd + nieuwe build

---
+++ Row The Boat/Assets/Scripts/PhotonNetworking/RoeiButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.PhotonNetworking
+{
+	class RoeiButtonHandler : MonoBehaviour
+	{
+		public PhotonRoeier Roeier;
+
+		public void ButtonClick()
+		{
+			if (this.Roeier == null) this.SetRoeier();
+			this.Roeier.Roei(20f);
+		}
+
+
+		private void SetRoeier()
+		{
+			foreach (GameObject go in GameObject.FindGameObjectsWithTag("PhotonPlayer"))
+				if (go.GetComponent<PhotonRoeier>().PaddleViewId != 0)
+				{
+					Roeier = go.GetComponent<PhotonRoeier>();
+					break;
+				}
+		}
+
+	}
+}
